b76cda5767ae4702cdd0b770b23a68fe8ada435d
Daud Hidayat R <daud28ramadhan@gmail.com>
2023-09-27 05:05:12 +0700

    belajar Operasi Perbandingan
    Signed-off-by: Daud Hidayat R <daud28ramadhan@gmail.com>

---
+++ src/main/kotlin/OperasiPerbandingan.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val a = 100
+    val b = 200
+    val result: Boolean = a > b
+    println(result)
+    println(a < b)
+    println(a >= b)
+    println(a <= b)
+    println(a == b)
+    println(a != b)
+}
