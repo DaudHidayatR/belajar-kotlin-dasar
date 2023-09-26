8859504338149b63fc60f997c74843dd5422c577
Daud Hidayat R <daud28ramadhan@gmail.com>
2023-09-26 18:17:15 +0700

    belajar Variable
    Signed-off-by: Daud Hidayat R <daud28ramadhan@gmail.com>

---
+++ src/main/kotlin/Variable.kt
@@ -0,0 +1,12 @@
+const val APPLICATION = "Belajar Kotlin Dasar"
+const val VERSION = "0.0.1"
+fun main() {
+    val firstName = "Daud"
+    val age = 20
+    val name: String? = null
+
+    println(firstName)
+    println(age)
+    println(name)
+    println("$APPLICATION : $VERSION")
+}
