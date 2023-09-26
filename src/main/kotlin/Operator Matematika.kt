1c75abcb461d341c5a005d43f6dde11926e9e70f
Daud Hidayat R <daud28ramadhan@gmail.com>
2023-09-27 05:00:43 +0700

    belajar Operator Matematika
    Signed-off-by: Daud Hidayat R <daud28ramadhan@gmail.com>

---
+++ src/main/kotlin/Operator Matematika.kt
@@ -0,0 +1,19 @@
+fun main() {
+    val result = 10 + 10 / 2
+    println(result)
+
+    val result2 = (10 + 10) / 2
+    println(result2)
+
+//    Augmented Assignments
+    var total = 0
+    val barang1 = 100
+    total += barang1
+    val barang2 = 200
+    total += barang2
+    val barang3 = 300
+    total += barang3
+    println(total)
+    total++
+    println(total)
+}
