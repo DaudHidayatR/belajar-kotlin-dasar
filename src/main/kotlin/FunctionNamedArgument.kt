6788d673e85e4a9605cf6a728dce474ed9cb8372
Daud Hidayat R <daud28ramadhan@gmail.com>
2023-09-27 11:33:00 +0700

    belajar Function Named Argument
    Signed-off-by: Daud Hidayat R <daud28ramadhan@gmail.com>

---
+++ src/main/kotlin/FunctionNamedArgument.kt
@@ -0,0 +1,14 @@
+fun fullname(
+    firstName: String,
+    middleName: String,
+    lastName: String
+){
+    println("Hello $firstName $middleName $lastName")
+}
+fun main() {
+    fullname(
+        firstName = "Daud",
+        middleName = "Hidayat",
+        lastName = "Ramadhan")
+
+}
