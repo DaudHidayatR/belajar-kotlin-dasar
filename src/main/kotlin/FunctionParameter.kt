2dd0b9e3f77a22e455735337f9bb4bd137bc7a8e
Daud Hidayat R <daud28ramadhan@gmail.com>
2023-09-27 10:56:42 +0700

    belajar Function Parameter
    Signed-off-by: Daud Hidayat R <daud28ramadhan@gmail.com>

---
+++ src/main/kotlin/FunctionParameter.kt
@@ -0,0 +1,9 @@
+fun sayHello(firstName: String, lastName: String){
+    println("Hello $firstName $lastName")
+}fun sayHello(Name: String,){
+    println("Hello $Name")
+}
+fun main() {
+    sayHello("daud", "hidayat")
+    sayHello("daud")
+}
