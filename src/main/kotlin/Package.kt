2e101857545207f0c459e1c932df54c49a1c89eb
Daud Hidayat R <daud28ramadhan@gmail.com>
2023-09-28 16:13:25 +0700

    belajar Package dan Import
    Signed-off-by: Daud Hidayat R <daud28ramadhan@gmail.com>

---
+++ src/main/kotlin/Package.kt
@@ -0,0 +1,7 @@
+import com.sagash.belajar.util.sayHello
+import com.sagash.belajar.util.sayGoodbye
+
+fun main() {
+    println(sayHello("Sagash"))
+    println(sayGoodbye("Sagash"))
+}
---
+++ src/main/kotlin/com/sagash/belajar/util/StringUtil.kt
@@ -0,0 +1,9 @@
+package com.sagash.belajar.util
+
+fun sayHello(name: String): String {
+    return "Hello $name"
+}
+fun sayGoodbye(name: String): String {
+    return "Goodbye $name"
+}
+
